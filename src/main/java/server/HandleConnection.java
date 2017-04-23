b2b05c9c2cfc712200862790bcc9c02134aaa421
HermanMeier <herman.meier@hotmail.com>
2017-04-23 14:41:24 +0300

    Parandasin oma eilseid muudatusi kliendile ja serverile. Puhastasin koodi ja püütsin teha kõike loetavamaks. Testisin kliendi ja serveri suhtlust.

--- src/main/java/server/HandleConnection.java
+++ src/main/java/server/HandleConnection.java
@@ -7,60 +7,37 @@ import org.jdom2.JDOMException;
 import java.io.*;
 import java.net.Socket;
 import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.List;
-
-/*
-Võtab sisse ühe ühenduse ja selle ühenduse peal
-võtab vastu käske ja tegeleb nendega
- */
 
 public class HandleConnection implements Runnable {
     private final Socket sock;
     private static final int BUFFER_SIZE=1024;
-    private static final List<String> commands = Arrays.asList("?", "db", "xml", "edit", "exit");
-    private SQLWriter sql = null;
+    private SQLWriter db = null;
     private XMLhandler xml = null;
 
-
-
-    public HandleConnection(Socket sock) {
+    HandleConnection(Socket sock) {
         this.sock = sock;
     }
 
+    @Override
     public void run() {
 
         try (DataInputStream dis = new DataInputStream(sock.getInputStream());
              DataOutputStream dos = new DataOutputStream(sock.getOutputStream())) {
-
-/*            File file;
-            if (dis.readUTF().equals("sending file")){
-                file=receiveFile(dis);
-            }
-            else if(dis.readUTF().equals("sending URL")){
-
-                //TODO convert URL to file
-            }*/
-
-
-            while (true){
-                String command=receiveCommand(dis);
+            while (true) {
+                String command = receiveCommand(dis);
 
                 switch (command) {
                     case "?":
-                        dos.writeBoolean(true);
                         break;
                     case "db":
-                        dos.writeBoolean(true);
-
                         String[] DBinfo = new String[4];
                         for (int i = 0; i < DBinfo.length; i++) {
                             DBinfo[i] = dis.readUTF();
                         }
 
                         try {
-                            sql = new SQLWriter(DBinfo[0], DBinfo[1], DBinfo[2], DBinfo[3]);
-                            sql.connectToDB();
+                            db = new SQLWriter(DBinfo[0], DBinfo[1], DBinfo[2], DBinfo[3]);
+                            db.connectToDB();
                             dos.writeUTF("Connected to database.");
                         } catch (SQLException e) {
                             dos.writeUTF("Failed to connect to database.");
@@ -68,41 +45,40 @@ public class HandleConnection implements Runnable {
 
                         break;
                     case "xml":
-                        dos.writeBoolean(true);
-
                         File file = null;
                         String answer = dis.readUTF();
-                        if (answer.equals("sending file")){
-                            file=receiveFile(dis);
-                        }
-                        else if(answer.equals("sending URL")){
+                        if (answer.equals("file")) {
+                            if (dis.readBoolean()) {
+                                file = receiveFile(dis);
+                                dos.writeUTF("File received.");
+                            } else
+                                break;
+                        } else if (answer.equals("sending URL")) {
                             //TODO convert URL to file
                         }
 
-                        if (!answer.equals(""))  {
+                        if (!answer.equals("")) {
                             try {
                                 xml = new XMLhandler(file.getName());
                                 xml.openXML();
                                 dos.writeUTF("Opened XML document.");
                             } catch (JDOMException e) {
-                                dos.writeUTF("Filed to open XML document.");
+                                dos.writeUTF("Filed to open XML document. Invalid path or url.");
                             }
                         }
 
                         break;
                     case "edit":
-                        dos.writeBoolean(true);
-
                         String db = dis.readUTF();
                         String table = dis.readUTF();
                         String xmlName = dis.readUTF();
 
-                        if (!sql.getDbHost().equals(db)) {
+                        if (!this.db.getDbHost().equals(db)) {
                             dos.writeBoolean(false);
                             dos.writeUTF("Not connected to database.");
                             break;
                         }
-                        if (!sql.getTableNames().contains(table))   {
+                        if (!this.db.getTableNames().contains(table)) {
                             dos.writeBoolean(false);
                             dos.writeUTF("No such table in database.");
                             break;
@@ -118,32 +94,17 @@ public class HandleConnection implements Runnable {
                         //TODO vaja see edit mode valmis teha, server peaks kasutama editDatabase klassi. xml ja ab sisu saatmiseks on osa koodi ui klassi commentis olemas
                         break;
                     case "exit":
-                        dos.writeBoolean(true);
                         break;
-                    default:
-                        dos.writeBoolean(false);
                 }
             }
-
-
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (SQLException e) {
+        } catch (IOException | SQLException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                sock.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
         }
     }
 
     private static String receiveCommand(DataInputStream dis) throws IOException {
         try {
-            String command=dis.readUTF();
-            return command;
+            return dis.readUTF();
         } catch (IOException e) {
             System.out.println("Error receiving command");
             e.printStackTrace();
@@ -153,28 +114,23 @@ public class HandleConnection implements Runnable {
 
 
     private static File receiveFile(DataInputStream dis) throws IOException {
-        try {
-            String fileName = dis.readUTF();
-            long fileSize = dis.readLong();
+        String fileName = dis.readUTF();
+        long fileSize = dis.readLong();
 
-            File file = new File(fileName);
-            file.createNewFile();
+        File file = new File(fileName);
+        file.createNewFile();
 
-            if (fileSize>0) {
-                try (FileOutputStream fos = new FileOutputStream(file)) {
-                    byte[] buffer = new byte[BUFFER_SIZE];
-                    int count;
-                    while ((count = dis.read(buffer, 0, (int) Math.min(buffer.length, fileSize))) != -1 && fileSize > 0) {
-                        fos.write(buffer, 0, count);
-                        fileSize -= count;
-                    }
+        if (fileSize>0) {
+            try (FileOutputStream fos = new FileOutputStream(file)) {
+                byte[] buffer = new byte[BUFFER_SIZE];
+                int count;
+                while ((count = dis.read(buffer, 0, (int) Math.min(buffer.length, fileSize))) != -1 && fileSize > 0) {
+                    fos.write(buffer, 0, count);
+                    fileSize -= count;
                 }
             }
-            return file;
         }
-        catch (Exception e){
-            throw e;
-        }
+        return file;
     }
 }
 
