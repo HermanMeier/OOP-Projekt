5d6aa3c9cfbd379c83a74181336773a3c3038cd8
villemtonisson <villemtonisson@gmail.com>
2017-05-16 10:42:34 +0300

    Enne jäi üks klass lisamata

---
+++ src/main/java/client/commands/CommandInsert.java
@@ -0,0 +1,30 @@
+package client.commands;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by Villem on 16/05/2017.
+ */
+public class CommandInsert extends BaseCommand implements Command {
+    CommandInsert(DataOutputStream toServer, DataInputStream fromServer, List<String> args) {
+        super(toServer, fromServer, args);
+    }
+
+    @Override
+    public void beforeSend() {
+
+    }
+
+    @Override
+    public void send() throws IOException {
+        sendCommand("insert", args);
+    }
+
+    @Override
+    public void afterSend() throws IOException {
+        System.out.println(fromServer.readUTF());
+    }
+}
