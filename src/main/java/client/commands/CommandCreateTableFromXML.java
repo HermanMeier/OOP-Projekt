fa18c050e832193581c37f78aad45cc0acdef4e7
Herman Meier <herman.meier@estcube.eu>
2017-05-17 19:35:33 +0300

    CreateTable käsk

---
+++ src/main/java/client/commands/CommandCreateTableFromXML.java
@@ -0,0 +1,17 @@
+package client.commands;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+public class CommandCreateTableFromXML extends BaseCommand {
+  CommandCreateTableFromXML(DataOutputStream toServer, DataInputStream fromServer, String command, List<String> args, String message) {
+    super(toServer, fromServer, command, args, message);
+  }
+
+  @Override
+  public void afterSend() throws IOException {
+
+  }
+}
